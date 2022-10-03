efb349bd1f4f8627b76dddcc663d4b7786883adb
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-03 20:04:18 +0900

    const : 定数について

---
+++ Sample206/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// const : 定数
+const int NUMBER = 100;
+const string STRING = "Hoge";
+Console.WriteLine( NUMBER );
+Console.WriteLine( STRING );
