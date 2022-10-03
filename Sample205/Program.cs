592179ce3a265751f41283e7248d6cc777ebef23
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-03 20:00:40 +0900

    文字列の変数について

---
+++ Sample205/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string str1, str2;
+Console.Write( "str1=" );
+str1 = Console.ReadLine();
+Console.Write( "str2=" );
+str2 = Console.ReadLine();
+Console.WriteLine( "str1 + str2 = {0}", str1 + str2 );
