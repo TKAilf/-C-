c999d1bb18aaa2a159c4909722a7a72ebcf6aa93
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-03 19:29:16 +0900

    四則演算について

---
+++ Sample201/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("{0} + {1} = {2}", 6, 2, 6 + 2);
+Console.WriteLine("{0} - {1} = {2}", 6, 2, 6 - 2);
+Console.WriteLine("{0} * {1} = {2}", 6, 2, 6 * 2);
+Console.WriteLine("{0} / {1} = {2} 余り {3}", 6, 2, 6 / 2, 6 % 2);
