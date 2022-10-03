e500fc7cd33b8d0109b29effbf6539b40ead98aa
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-03 20:24:16 +0900

    例題

---
+++ Exercise202/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b, c;
+a = 2;
+b = 3;
+c = a - b;
+Console.WriteLine( "{0} + {1} = {2}", 2, 3, 2 + 3 );
+Console.WriteLine( c );
---
+++ Exercise203/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1, s2, s3;
+s1 = "あいう";
+s2 = "えお";
+s3 = s1 + s2;
+Console.WriteLine( s3 );
