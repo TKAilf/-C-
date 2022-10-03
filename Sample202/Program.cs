8b16f605cd66424ae0cd18c88d1e7d36becfcae9
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-03 19:36:17 +0900

    変数について

---
+++ Sample202/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a;
+int b = 3;
+int add, sub;
+double avg;
+a = 6;
+add = a + b;
+sub = a - b;
+avg = ( a + b ) / 2.0;
+Console.WriteLine( "{0} + {1} = {2}", a, b, add );
+Console.WriteLine( "{0} - {1} = {2}", a, b, sub );
+Console.WriteLine( "{0}と{1}の平均値{2}", a, b, avg );
