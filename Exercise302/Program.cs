5a9bb3542f659c88c25e2c8dab1b6c04ddc01721
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-04 20:17:12 +0900

    練習問題：条件分岐

---
+++ Exercise302/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+if( s1 == "hello" && s2 == "world" ){
+    Console.WriteLine( "Good" );
+}else{
+    Console.WriteLine( "helloworld please!" );
+}
---
+++ Exercise303/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse( Console.ReadLine() );
+switch( n ){
+    case 1:
+        Console.WriteLine( "ONE" );
+        break;
+    case 2:
+        Console.WriteLine( "TWO" );
+        break;
+    case 3:
+        Console.WriteLine( "THREE" );
+        break;
+    default:
+        Console.WriteLine( "範囲外" );
+        break;
+}
