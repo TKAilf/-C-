344672f4b0f253a32ea4c9d2adada6539ae4d76b
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-05 18:51:21 +0900

    ループ文：練習問題

---
+++ Exercise402/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("整数を入力:");
+int num = int.Parse( Console.ReadLine() );
+for( int i = 1; i < num + 1; i++ ){
+    if( num % i == 0 ){
+        Console.Write( i + " " );
+    }
+}
+Console.WriteLine();
---
+++ Exercise403/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random num1 = new Random();
+Random num2 = new Random();
+int a = num1.Next( 1, 10 );
+int b = num2.Next( 1, 10 );
+Console.WriteLine( "a={0} b={1}", a, b );
+while( true ){
+    Console.Write( "a+b=" );
+    int nInput = int.Parse( Console.ReadLine() );
+    if( nInput == ( a + b ) ){
+        Console.WriteLine( "正解" );
+        break;
+    }else{
+        Console.WriteLine( "間違い" );
+    }
+}
