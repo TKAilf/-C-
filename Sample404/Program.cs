610ba111d4f20ae034f99ac6ba797aeeb6268245
峻輝 川口 <ktoshiki0511@yahoo.co.jp>
2022-10-05 18:34:10 +0900

    for,whileループ文

---
+++ Sample404/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 0;
+do{
+    Console.Write( i + " " );
+    i++;
+} while( i <= 5 );
+Console.WriteLine();
---
+++ Sample406/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random();
+Console.WriteLine( "6が出たら終了" );
+while( true ){
+    int dice = random.Next( 1, 7 );
+    Console.WriteLine( dice );
+    if( dice == 6 ){
+        break;
+    }
+}
+Console.WriteLine( "終了" );
